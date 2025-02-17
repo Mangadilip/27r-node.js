1aa6aad40a5bdf964cf69a580417156b39964fae
mangadilip <mangadileep42@gmail.com>
2025-02-17 09:07:07 +0530

    first commit

---
+++ nodejs-1/main.js
@@ -0,0 +1,4 @@
+const obj = require("./module1")
+console.log("iam main module")
+console.log(obj.fun1())
+console.log(obj.x)
---
+++ nodejs-1/module1.js
@@ -0,0 +1,6 @@
+function studentInfo(){
+    console.log("hello student");
+    return "hiii";
+}
+var x = "dilip";
+module.exports = {fun1 : studentInfo, x : x};
