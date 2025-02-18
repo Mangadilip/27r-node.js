a31b57a57d6286a40841b21df264aacf1ea1f0a8
mangadilip <mangadileep42@gmail.com>
2025-02-18 09:06:39 +0530

    updated

--- nodejs-1/main.js
+++ nodejs-1/main.js
@@ -1,5 +1,16 @@
-const obj = require("./module1")
-console.log("iam main module")
-console.log(obj.fun1())
-console.log(obj.x)
-console.log("whats new")
+// const obj = require("./module1")
+// console.log("iam main module")
+// console.log(obj.fun1())
+// console.log(obj.x)
+// console.log("whats new")
+
+const http = require("http")
+
+
+const server = http.createServer((req, res)=>{
+    res.write("hello world")
+    res.end("")
+});
+server.listen("3102",()=>{
+    console.log("server is running")
+})
