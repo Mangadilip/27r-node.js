d08b160782907afabef7a5dab2b988378964ca55
mangadilip <mangadileep42@gmail.com>
2025-02-19 20:57:09 +0530

    task update

--- nodejs-1/main.js
+++ nodejs-1/main.js
@@ -4,11 +4,6 @@
 // console.log(obj.x)
 // console.log("whats new")
 
-
-
-
-
-
 // const server = http.createServer((req, res)=>{
 //     res.write("welcome to my new project")
 //     res.end("")
@@ -17,56 +12,58 @@
 //     console.log("server is running")
 // })
 
-
-
 // movie 19-02-25----------------------------------------------------
-const http =require("http");
+const http = require("http");
 const url = require("url");
 
-
 const server = http.createServer((req, res) => {
-  
-const parsedUrl =  url.parse(req.url, true);
-// console.log(parsedUrl)
-const ticket = parsedUrl.query.ticket
+  const parsedUrl = url.parse(req.url, true);
+  // console.log(parsedUrl)
+  const quantity = parsedUrl.query.quantity;
 
- if(req.method == "GET"){
-  if(parsedUrl.pathname == "/rebel"){
-    res.writeHead(200,"ok",{"content-type" : "application/json"})
-    res.write(JSON.stringify({"movie title" : "rebel",screen : "one",tickets:ticket}))
-    res.end()
+  if (req.method == "GET") {
+    if (parsedUrl.pathname == "/veg/gobi") {
+      res.writeHead(200, "ok", { "content-type": "application/json" });
+      res.write(
+        JSON.stringify({
+          item: "gobi",
+          quantity: quantity,
+          price: "2 x 150",
+          gst: "5%",
+          totalPrice: "price + gst",
+          message: "thankyou for order",
+        })
+      );
+      res.end();
+    } 
+    else if (parsedUrl.pathname == "/nonveg/chicken") {
+      res.writeHead(200, "ok", { "content-type": "application/json" });
+      res.write(
+        JSON.stringify({
+          item: "chicken",
+          quantity: quantity,
+          price: "2 x 150",
+          gst: "5%",
+          totalPrice: "price + gst",
+          message: "thankyou for order",
+        })
+      );
+      res.end();
+    } else {
+      res.writeHead(400, "not found", { "content-type": "application/json" });
+      res.write(JSON.stringify({ message: " use endpoint as a /veg/gobi , /nogveg/chicken ,/veg/gobi?quantity=2, /nonveg/chicken?quantity=2" }));
+      res.end();
+    }
+  } else if (req.method == "POST") {
+    res.end();
+  } else if (req.method == "PUT") {
+    res.end();
+  } else if (req.method == "DELETE") {
+    res.end();
+  } else {
+    res.end();
   }
-  else if(parsedUrl.pathname == "/chaava"){
-    res.writeHead(200,"ok",{"content-type" : "application/json"})
-    res.write(JSON.stringify({"movie title" : "chaava",screen : "two",tickets:ticket}))
-    res.end()
-  }
-  else if(parsedUrl.pathname == "/billa"){
-    res.writeHead(200,"ok",{"content-type" : "application/json"})
-    res.write(JSON.stringify({"movie title" : "billa",screen : "three",tickets:ticket}))
-    res.end()
-  }
-  else{
-    res.writeHead(400,"not found",{"content-type" : "application/json"})
-    res.write(JSON.stringify({message : "movie not showing now"}))
-    res.end()
-  }
- }
- else if(req.method == "POST"){
-  res.end()
- }
- else if(req.method == "PUT"){
-  res.end()
- }
- else if(req.method == "DELETE"){
-  res.end()
- }
- else{
-  res.end()
- }
-  
 });
 server.listen("3101", () => {
   console.log("server is running");
 });
-
