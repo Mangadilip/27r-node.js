0feaa19652b57397a3917036c7e5fee62458f819
mangadilip <mangadileep42@gmail.com>
2025-02-20 11:30:57 +0530

    updated

--- nodejs-1/main.js
+++ nodejs-1/main.js
@@ -14,44 +14,106 @@
 
 // movie 19-02-25----------------------------------------------------
 const http = require("http");
+
 const url = require("url");
 
 const server = http.createServer((req, res) => {
   const parsedUrl = url.parse(req.url, true);
   // console.log(parsedUrl)
   const quantity = parsedUrl.query.quantity;
+  const gst = 0.18
 
   if (req.method == "GET") {
-    if (parsedUrl.pathname == "/veg/gobi") {
+    if (parsedUrl.pathname == "/menu") {
       res.writeHead(200, "ok", { "content-type": "application/json" });
       res.write(
         JSON.stringify({
-          item: "gobi",
-          quantity: quantity,
-          price: "2 x 150",
-          gst: "5%",
-          totalPrice: "price + gst",
-          message: "thankyou for order",
+          veg: [{ gobi: "150rs", panner: "200rs" }],
+          nonveg: [{ chicken: "250rs", fish: "260rs" }],
+          message: "requesting you to place the order",
         })
       );
       res.end();
-    } 
-    else if (parsedUrl.pathname == "/nonveg/chicken") {
-      res.writeHead(200, "ok", { "content-type": "application/json" });
-      res.write(
-        JSON.stringify({
-          item: "chicken",
-          quantity: quantity,
-          price: "2 x 150",
-          gst: "5%",
-          totalPrice: "price + gst",
-          message: "thankyou for order",
-        })
-      );
-      res.end();
-    } else {
+    } else if (parsedUrl.pathname == "/menu/veg") {
+      if (parsedUrl.query.item == "panner") {
+        price=quantity * 150
+        res.writeHead(200, "ok", { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            item: parsedUrl.query.item,
+            quantity: quantity,
+            price: price,
+            gst: price*gst,
+            totalPrice: price + gst,
+            message: "thankyou for order",
+          })
+        );
+        res.end();
+      } else if (parsedUrl.query.item == "gobi") {
+
+        price=quantity * 200
+        res.writeHead(200, "ok", { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            item: parsedUrl.query.item,
+            quantity: quantity,
+            price: price,
+            gst: price*gst,
+            totalPrice: price + gst,
+            message: "thankyou for order",
+          })
+        );
+        res.end();
+      }else{
+        res.writeHead(400, "not found", { "content-type": "application/json" });
+        res.write(JSON.stringify({message : "items not found"}))
+        res.end()
+      }
+    }
+    else if (parsedUrl.pathname == "/menu/nonveg") {
+      if (parsedUrl.query.item == "chicken") {
+        price=quantity*250
+        res.writeHead(200, "ok", { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            item: parsedUrl.query.item,
+            quantity: quantity,
+            price:price,
+            gst: price*gst,
+            totalPrice: price + gst,
+            message: "thankyou for order",
+          })
+        );
+        res.end();
+      } else if (parsedUrl.query.item== "fish") {
+        price = quantity * 260;
+       
+        res.writeHead(200, "ok", { "content-type": "application/json" });
+        res.write(
+          JSON.stringify({
+            item: parsedUrl.query.item,
+            quantity: quantity,
+            price: price,
+            gst: price * gst,
+            totalPrice: price + gst,
+            message: "thankyou for order",
+          })
+        );
+        res.end();
+      }else{
+        res.writeHead(400, "not found", { "content-type": "application/json" });
+        res.write(JSON.stringify({message : "items not found"}))
+        res.end()
+      }
+    }
+    else {
       res.writeHead(400, "not found", { "content-type": "application/json" });
-      res.write(JSON.stringify({ message: " use endpoint as a /veg/gobi , /nogveg/chicken ,/veg/gobi?quantity=2, /nonveg/chicken?quantity=2" }));
+      res.write(
+        JSON.stringify({
+          message:
+            " use endpoint ",
+        })
+      );
       res.end();
     }
   } else if (req.method == "POST") {
